3ab923c28f1a7698d6e426c48ce58b3f0eee324b
Andreas Ommundsen <anthomm@live.com>
2023-03-17 12:04:33 +0100

    fix: add human-readable hint for QueryTimeFormat;

--- constants/time_formats.go
+++ constants/time_formats.go
@@ -1,3 +1,6 @@
 package constants
 
 const QueryTimeFormat = "2006-01-02_15:04"
+
+// QueryTimeHint is the human-readable explanation of QueryTimeFormat. Intended for use with error reporting.
+const QueryTimeHint = "YYYY-MM-DD_hh:mm"
