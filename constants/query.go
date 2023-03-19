ffbf563ba018490338b2d8d77bd5b87e5eeb9fc6
Andreas Ommundsen <anthomm@live.com>
2023-03-19 15:29:42 +0100

    fix: refactor query constants;

--- constants/query.go
+++ constants/query.go
@@ -4,3 +4,12 @@ const QueryTimeFormat = "2006-01-02_15:04"
 
 // QueryTimeHint is the human-readable explanation of QueryTimeFormat. Intended for use with error reporting.
 const QueryTimeHint = "YYYY-MM-DD_hh:mm"
+
+var ValidRelationalOperators = map[string]bool{
+	"EQ": true,
+	"NE": true,
+	"LT": true,
+	"GT": true,
+	"LE": true,
+	"GE": true,
+}
